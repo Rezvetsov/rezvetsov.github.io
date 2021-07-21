3baf3a76641fc587cad0e97c021b6aff9fe468a6
Rezvetsov Artyom <rezvetz@gmail.com>
2021-07-21 21:21:50 +0300

    init

---
+++ _nuxt/static/1626891008/services/_guard_of_organizations/guard_of_medical_institutions/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/services/_guard_of_organizations/guard_of_medical_institutions", {data:[{}],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1626891008/services/_guard_of_real_estate/guard_private_property/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/services/_guard_of_real_estate/guard_private_property", {data:[{}],fetch:[],mutations:void 0});
