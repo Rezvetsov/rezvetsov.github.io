a75f17e40fa1f2f5ee2490640eb669c319122cd3
Rezvetsov Artyom <rezvetz@gmail.com>
2022-05-17 19:28:24 +0300

    1

---
+++ _nuxt/static/1652804665/services/_guard_of_organizations/guard_of_educational_institutions/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/services/_guard_of_organizations/guard_of_educational_institutions", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1652804665/special_equipment/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/special_equipment", {data:[{}],fetch:{},mutations:void 0});
